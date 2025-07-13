a6133cf8dd04695fda65e2938f4bd21de97eed41
Ubuntu <ubuntu@ip-172-31-19-181.ca-central-1.compute.internal>
2025-07-13 01:09:19 +0000

    ssl certificate update

--- terraform-ssl-alb/main.tf
+++ terraform-ssl-alb/main.tf
@@ -1,8 +1,8 @@
 module "ssl_cert" {
   source         = "./modules/ssl_cert_dns_record"
-  domain_name    = "your.domain.com"
-  hosted_zone_id = "Z123456ABCEXAMPLE"
-  elb_name       = "your-elb-name"
+  domain_name    = "dev.oluwaseunalade.com"
+  hosted_zone_id = "Z01920241TQU6SU23PN1G"
+  elb_name       = "a5e36b9b49de3466388a8545f9f95c88"
   tags = {
     Environment = "dev"
     Owner       = "Oluwaseun"
--- terraform-ssl-alb/modules/ssl_cert_dns_record/main.tf
+++ terraform-ssl-alb/modules/ssl_cert_dns_record/main.tf
@@ -10,24 +10,40 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "validation" {
-  name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      value  = dvo.resource_record_value
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
   zone_id = var.hosted_zone_id
-  records = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
+  records = [each.value.value]
   ttl     = 300
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.validation.fqdn]
+  certificate_arn = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [
+    for record in aws_route53_record.validation : record.fqdn
+  ]
 }
 
-resource "aws_elb_listener" "https_listener" {
-  count              = var.elb_name != null ? 1 : 0
-  elb_name           = var.elb_name
-  instance_port      = 80
-  instance_protocol  = "HTTP"
-  lb_port            = 443
-  lb_protocol        = "HTTPS"
-  ssl_certificate_id = aws_acm_certificate.cert.arn
+resource "null_resource" "attach_ssl_listener" {
+  depends_on = [aws_acm_certificate_validation.cert_validation]
+
+  triggers = {
+    cert_arn = aws_acm_certificate.cert.arn
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      aws elb create-load-balancer-listeners \
+        --load-balancer-name ${var.elb_name} \
+        --listeners "Protocol=HTTPS,LoadBalancerPort=443,InstanceProtocol=HTTP,InstancePort=80,SSLCertificateId=${aws_acm_certificate.cert.arn}"
+    EOT
+  }
 }
